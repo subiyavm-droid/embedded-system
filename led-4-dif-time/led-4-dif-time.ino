adf9b01fa814a4477f24b03b89f4e0ee52089fd9
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:08:13 +0530

    Internship @ Emglitz

---
+++ led-4-dif-time/led-4-dif-time.ino
@@ -0,0 +1,26 @@
+void setup()  {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(3, 1);
+  delay(1000);
+  digitalWrite(3, 0);
+  delay(1000);
+  digitalWrite(4, 1);
+  delay(1000);
+  digitalWrite(4, 0);
+  delay(1000);
+  digitalWrite(5, 1);
+  delay(1000);
+  digitalWrite(5, 0);
+  delay(1000);
+  digitalWrite(6, 1);
+  delay(1000);
+  digitalWrite(6, 0);
+  delay(1000);
+}
+
