48b0dec3081331cde82c72e3003bec9863d437c8
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:11:07 +0530

    Internship @ Emglitz

---
+++ switch_8/switch_8.ino
@@ -0,0 +1,45 @@
+void setup() {
+ pinMode(1,INPUT);
+ pinMode(2,OUTPUT);
+ pinMode(3,OUTPUT);
+ pinMode(4,OUTPUT);
+ pinMode(5,OUTPUT);
+ pinMode(6,OUTPUT);
+ pinMode(7,OUTPUT);
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+ }
+
+void loop() {
+  int swt=digitalRead(1);
+  if(swt==1)
+  {
+    digitalWrite(2,1);
+    delay(1000);
+    digitalWrite(3,1);
+    delay(1000);
+    digitalWrite(4,1);
+    delay(1000);
+    digitalWrite(5,1);
+    delay(1000);
+    digitalWrite(6,1);
+    delay(1000);
+    digitalWrite(7,1);
+    delay(1000);
+    digitalWrite(8,1);
+    delay(1000);
+    digitalWrite(9,1);
+    delay(1000);
+  }
+  else
+  {
+   digitalWrite(2,0);
+   digitalWrite(3,0);
+   digitalWrite(4,0);
+   digitalWrite(5,0);
+   digitalWrite(6,0);
+   digitalWrite(7,0);
+   digitalWrite(8,0);
+   digitalWrite(9,0);
+  }
+}
