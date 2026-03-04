3e9319dca0601bfd0667c77b6f5a681e8bd36433
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:08:38 +0530

    Internship @ Emglitz

---
+++ led-6/led-6.ino
@@ -0,0 +1,31 @@
+void setup() {
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(3,1);
+  digitalWrite(5,1);
+  digitalWrite(7,1);
+  delay(1000);
+  digitalWrite(4,0);
+  digitalWrite(6,0);
+  digitalWrite(8,0);
+  delay(1000);
+  digitalWrite(3,0);
+  digitalWrite(5,0);
+  digitalWrite(7,0);
+  delay(1000);
+  digitalWrite(4,1);
+  digitalWrite(6,1);
+  digitalWrite(8,1);
+  delay(1000);
+  digitalWrite(4,0);
+  digitalWrite(6,0);
+  digitalWrite(8,0);
+  delay(1000);
+}
