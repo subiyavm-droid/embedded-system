ec6e1636ee4d7a98989ce4f08d4caa64670c4900
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:04:10 +0530

    Internship @ Emglitz

---
+++ lcd_scorll/lcd_scorll.ino
@@ -0,0 +1,12 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13,12,11,10,9,8);
+void setup() {
+  lcd.begin(16,2);
+}
+
+void loop() {
+ lcd.setCursor(0,0);
+ lcd.print("Abi");
+ lcd.scrollDisplayRight();
+ delay(1000);
+}
