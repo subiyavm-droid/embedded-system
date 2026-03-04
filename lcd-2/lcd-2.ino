e86a18dc007903b7fd33af6f738f9ac3d5a4b0a7
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:04:37 +0530

    Internship @ Emglitz

---
+++ lcd-2/lcd-2.ino
@@ -0,0 +1,13 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13,12,11,10,9,8);
+void setup() {
+  lcd.begin(16,2);
+}
+
+void loop() {
+ lcd.setCursor(0,0);
+ lcd.print("Abi");
+ lcd.setCursor(8,1);
+ lcd.print("shek");
+ 
+}
