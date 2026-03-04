62fc659e5b0b29877a4def341680b43dfd40f774
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:13:16 +0530

    Internship @ Emglitz

---
+++ temp-sensor/temp-sensor.ino
@@ -0,0 +1,16 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13,12,11,10,9,8);
+void setup() {
+ pinMode(A4,INPUT);
+ lcd.begin(16,2);
+}
+
+void loop() {
+  int temp = analogRead(A4);
+  temp = temp*0.4848;
+  lcd.setCursor(0,0);
+  lcd.print("Temperature");
+  lcd.setCursor(13,0);
+  lcd.print(temp);
+
+}
