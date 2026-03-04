f2f957f553b6eb5cda7bb2ca758932b466548dea
subiyavm-droid <subiyavm@gmail.com>
2026-03-04 17:10:39 +0530

    Internship @ Emglitz

---
+++ switch/switch.ino
@@ -0,0 +1,17 @@
+void setup() {
+ pinMode(2,INPUT);
+ pinMode(3,OUTPUT);
+}
+
+void loop() {
+  int swt=digitalRead(2);
+  if(swt==1)
+{
+  digitalWrite(3,1);
+}
+ else
+{
+  digitalWrite(3,0);
+}
+}
+
